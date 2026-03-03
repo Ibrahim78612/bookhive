21cd526e37c90fe64a08722037023266afefc5dc
Ibrahim Ilyas <3008614i@student.gla.ac.uk>
2026-03-03 03:37:58 +0000

    Added signups and login

--- bookhive/urls.py
+++ bookhive/urls.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('books/', include('books.urls'))
-
+    path("", include("core.urls")),
+    path("", include("users.urls")), 
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
-# Create your views here.
+def signup_view(request):
+    form = UserCreationForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect("/")
+    return render(request, "users/signup.html", {"form": form})
+
+def login_view(request):
+    form = AuthenticationForm(request, data=request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        login(request, form.get_user())
+        return redirect("/")
+    return render(request, "users/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
