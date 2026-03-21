e3dec0970877e377a8b67518e27f0df0ae416f55
frostyract <frostyerroroof@protonmail.com>
2026-03-21 20:14:40 +0000

    made books page use external stylesheet, also added files forgotten from last commit

---
+++ static/books/js/get_book_data.js
@@ -0,0 +1,42 @@
+// taken from stackoverflow to load jquery in external js file
+if (typeof jQuery === "undefined") {
+    var script = document.createElement('script');
+    script.src = 'http://code.jquery.com/jquery-latest.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+addSubject = function(currentValue) {
+  $(".tags")/append("<li>"+currentValue+"</li>");
+}
+
+window.onload = function() {
+    $.ajax({
+          url: '/books/json',
+          type: 'get',
+          data: {
+                work_id: $('.work_id').text(),
+                },
+          success: function(response) {
+                    $('.title').text(response.title);
+
+                    if (response.description != null) {
+                      $('.description').text(response.description);
+                    }
+
+                    if (response.authors != null) {
+                      $('.bookauthors').text(response.authors);
+                    }
+                    if (response.first_publish_date != null) {
+                      $('.year').text(response.first_publish_date);
+                    }
+                    if (response.cover != null) {
+                      $('.cover').attr("src", response.cover);
+                    }
+                    response.subjects.forEach(addSubject);
+                },
+          error: function() {
+                  window.location.href = "/";
+                }
+            });
+}
